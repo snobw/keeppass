358589ba34457164cee7be0ad52ce3610aec6c58
fballand <fballand.inope@gmail.com>
2017-03-11 00:14:57 +0100

    call dialog box to launch/save the blp file

--- keeppass.cpp
+++ keeppass.cpp
@@ -1,5 +1,7 @@
 #include "keeppass.h"
 #include "ui_keeppass.h"
+#include "qfiledialog.h"
+
 
 KeepPass::KeepPass(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +13,58 @@ KeepPass::KeepPass(QWidget *parent) :
 KeepPass::~KeepPass()
 {
     delete ui;
+}
+
+void KeepPass::on_leKey_textEdited(const QString &arg1)
+{
+    if(arg1!=""){
+        ui->btOK->setEnabled(true);
+    }else{
+        ui->btOK->setEnabled(false);
+    }
+}
+
+void KeepPass::on_btURL_clicked()
+{
+    ui->leURL->setText(QFileDialog::getOpenFileName(this, tr("Open File"),"/home", tr("base log (*.blp)")));
+}
+
+void KeepPass::on_btNewUrl_clicked()
+{
+    ui->leURL->setText(QFileDialog::getSaveFileName(this, tr("New File"),"/home", tr("base log (*.blp)")));
+}
+
+void KeepPass::on_btOK_clicked()
+{
+    //
+}
+
+void KeepPass::on_btNew_clicked()
+{
+    //
+}
+
+void KeepPass::on_btModified_clicked()
+{
+    //
+}
+
+void KeepPass::on_btDelete_clicked()
+{
+    //
+}
+
+void KeepPass::on_btCancel_clicked()
+{
+    //
+}
+
+void KeepPass::on_btAccept_clicked()
+{
+    //
+}
+
+void KeepPass::on_btSave_clicked()
+{
+    modelKeepPass._setFileName(ui->leURL->text());
 }
--- keeppass.h
+++ keeppass.h
@@ -2,6 +2,7 @@
 #define KEEPPASS_H
 
 #include <QMainWindow>
+#include "moduikeeppass.h"
 
 namespace Ui {
 class KeepPass;
@@ -15,8 +16,30 @@ public:
     explicit KeepPass(QWidget *parent = 0);
     ~KeepPass();
 
+private slots:
+    void on_leKey_textEdited(const QString &arg1);
+
+    void on_btURL_clicked();
+
+    void on_btNewUrl_clicked();
+
+    void on_btOK_clicked();
+
+    void on_btNew_clicked();
+
+    void on_btModified_clicked();
+
+    void on_btDelete_clicked();
+
+    void on_btCancel_clicked();
+
+    void on_btAccept_clicked();
+
+    void on_btSave_clicked();
+
 private:
     Ui::KeepPass *ui;
+    ModUIKeepPass modelKeepPass;
 };
 
 #endif // KEEPPASS_H
