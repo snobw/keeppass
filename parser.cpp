51e886728be4ddd217d5269a397b3480aa2cb7cf
fballand <fballand.inope@gmail.com>
2017-03-11 00:16:26 +0100

    generation class struct to parser and model

--- parser.cpp
+++ parser.cpp
@@ -4,4 +4,22 @@ Parser::Parser()
 {
 
 }
+Parser::_readParsing(QString path, QString txt, int index){
 
+}
+Parser::_writeParsing(QString path, QString txt, int index){
+// encode with binary struct XML
+/****
+ * idCat
+ * category
+ *
+ * idTitle
+ * Title
+ * Login
+ * Password
+ * comment
+ *
+ ***/
+
+
+}
--- parser.h
+++ parser.h
@@ -1,11 +1,15 @@
 #ifndef PARSER_H
 #define PARSER_H
+#include "qstring.h"
 
 
 class Parser
 {
 public:
     Parser();
+private:
+    bool _writeParsing(QString path,QString txt,int index=0);
+    bool _readParsing(QString path,QString txt,int index=0);
 };
 
 #endif // PARSER_H
