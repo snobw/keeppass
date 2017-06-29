250e03597130130212c3f43c61b36e06dd2ec8d9
snobw <fballand.inope@gmail.com>
2017-06-29 18:50:23 +0200

    add get and set functions to read and write the password struct

---
+++ password.cpp
@@ -0,0 +1,7 @@
+#include "password.h"
+
+Password::Password()
+{
+
+}
+
---
+++ password.h
@@ -0,0 +1,11 @@
+#ifndef PASSWORD_H
+#define PASSWORD_H
+
+
+class Password
+{
+public:
+    Password();
+};
+
+#endif // PASSWORD_H
